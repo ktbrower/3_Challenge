da0219751892db0bd3be65565cc753d0ccf9a453
Katherine Brower <ktbrower101@gmail.com>
2022-10-27 15:53:01 -0600

    add game

--- 4_Lab_2D/Assets/TextLabelBehaviour.cs
+++ 4_Lab_2D/Assets/TextLabelBehaviour.cs
@@ -1,6 +1,21 @@
+using System;
+using System.Globalization;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.UI;
+[RequireComponent(typeof(Text))]
 public class TextLabelBehaviour : MonoBehaviour
 {
  public Text label;
+ public FloatData dataobj;
+ private void Start()
+ {
+  label = GetComponent<Text>();
+  UpdateLabel();
+ }
+
+ public void UpdateLabel()
+ {
+  label.text = dataobj.value.ToString(CultureInfo.InvariantCulture);
+ }
 }
